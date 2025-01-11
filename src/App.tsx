bf4a5ae8b0d876e78bd92291a4ff04545844f357
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2025-01-11 22:59:37 +0530

    not found page created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {RootLayoutComponent} from "./components/layout/RootLayoutComponent.tsx";
+import {NotFoundComponent} from "./components/layout/NotFoundComponent.tsx";
 
 function App() {
+
+    const routes = createBrowserRouter([
+        {
+            path: '',
+            element : <RootLayoutComponent/>,
+            children : [
+            ]
+        },
+        {
+            path: "*",
+            element: <NotFoundComponent/>
+        }
+    ])
+
     return (
-    <>
-    </>
-  )
+        <>
+            <RouterProvider router={routes} />
+        </>
+    )
 }
 
 export default App
